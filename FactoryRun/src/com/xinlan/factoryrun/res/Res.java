b0b69ad461baeb419baa054e1044c08ff4436f59
siwangqishiq <525647740@qq.com>
2014-10-30 17:41:34 +0800

    push2

--- FactoryRun/src/com/xinlan/factoryrun/res/Res.java
+++ FactoryRun/src/com/xinlan/factoryrun/res/Res.java
@@ -1,9 +1,43 @@
 package com.xinlan.factoryrun.res;
 
-abstract public class Res
+import java.util.ArrayList;
+import java.util.List;
+
+import com.xinlan.factoryrun.task.Order;
+
+abstract class Res
 {
     protected String name;
     protected int id;
     protected int start;
     protected int end;
-}//end class
+    private List<Record> recordList = new ArrayList<Record>();
+
+    /**
+     * use it tools
+     * 
+     * @param time
+     */
+    public void use(Order order, int time)
+    {
+        Record record = new Record();
+        record.order = order;
+        record.orderStartTime = end;
+        record.orderEndTime = end + time;
+        recordList.add(record);
+        
+        end += time;
+    }
+
+    public void clear()
+    {
+        recordList.clear();
+    }
+    
+    private final class Record
+    {
+        Order order;
+        int orderStartTime;
+        int orderEndTime;
+    }// end inner class
+}// end class
--- FactoryRun/src/com/xinlan/factoryrun/task/Order.java
+++ FactoryRun/src/com/xinlan/factoryrun/task/Order.java
@@ -5,13 +5,17 @@ import java.util.HashSet;
 public class Order
 {
     private String id;
-    private int hashCode;
+    private int deadLine;
     private HashSet<Task> taskList =new HashSet<Task>();
-    public Order(String id){
+    
+    public Order(String id,int deadLine){
         this.id = id;
-        hashCode = id.hashCode();
+        this.deadLine = deadLine;
     }
+    
     public void submit(Task task){
         taskList.add(task);
     }
+    
+    
 }//end class
