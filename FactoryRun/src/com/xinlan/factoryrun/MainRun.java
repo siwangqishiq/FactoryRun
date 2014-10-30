8af8c274997b90c0459f35d8e0ef924a9d67a7e7
siwangqishiq <525647740@qq.com>
2014-10-30 15:50:41 +0800

    init

---
+++ FactoryRun/src/com/xinlan/factoryrun/MainRun.java
@@ -0,0 +1,13 @@
+package com.xinlan.factoryrun;
+
+public class MainRun
+{
+    /**
+     * main run...
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+}//end class
